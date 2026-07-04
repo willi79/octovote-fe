589a19802da905baf5810e316e74db5c4c2b2cf1
willi79 <willigeraldy@gmail.com>
2026-07-04 15:25:03 +0700

    style: shared navbar and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AuthService } from './core/service/auth.service';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, NavbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+    readonly auth = inject(AuthService);
+}
